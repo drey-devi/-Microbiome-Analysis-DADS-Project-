7f4c739eafec6b1580b3dec71f6df63ca3e3e82f
drey-devi <drey.devi2002@gmail.com>
2025-11-28 10:51:53 -0700

    first commit

---
+++ module12346.R
@@ -0,0 +1,59 @@
+# EXERCISE 1
+#In your console copy/paste the text below. Execute the command below in your terminal and describe the results.
+
+1:20
+
+seq(1:20)
+
+seq(0, 10, by=0.5)
+
+a<-seq(0, 10, by=0.5)
+
+length (a)
+
+rep(0, times = 10)
+
+#EXERCISE 2
+
+#Let's apply what we learned in tutorial #4.
+
+#1. Which one of the commands below will give you a true
+
+(3 > 5) & (4 == 4)
+
+(5 >3 ) & (4 == 4)
+
+#2. create a vector of words using your name and a sentence 
+
+
+#3. run the command "paste(my_vector, collapse = " ")". Describe what you see
+
+
+#so now the length of the new vector is ONE
+
+#4. Concatenate another word in that vector
+
+#EXERCISE 3
+##Instructions: answer the questions below 
+
+#1. create a vector "a" with 20 random value using the sample function from a vector of 100
+#if you need to: look up the sample function in the help tab
+
+#2. look at the first 10 elements of the vector
+
+#3. What will a[a>20] give you? Store it in a vector called b
+
+#4. What is the length of the vector b?
+
+#5. Create the vector below:
+y<-c(NA, 1:4, NA, NA, 5:18, rep(NA,5))
+
+#How would you select in one command all the values DIFFERENT from NA?
+
+#6. Within the same vector: how can you select the value above 5?
+
+#7. And now: both different than Na and above 5?
+
+#8. Subset element 5,6, and 8 from your vector
+
+#9. Gives all elements of y EXCEPT for the 2nd and 10 elements
